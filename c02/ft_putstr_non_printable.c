8e5690dc6a5304d827c6d4f1e90b2c67f7343add
raquel-1 <rocamartinezraquel@gmail.com>
2025-11-16 19:32:36 +0100

    ej 11 de c02

--- c02/ft_putstr_non_printable.c
+++ c02/ft_putstr_non_printable.c
@@ -6,11 +6,37 @@
 /*   By: raqroca- <raqroca-@student.42.madrid.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 18:26:31 by raqroca-          #+#    #+#             */
-/*   Updated: 2025/11/16 18:28:07 by raqroca-         ###   ########.fr       */
+/*   Updated: 2025/11/16 19:32:18 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 void	ft_putstr_non_printable(char *str)
 {
-	
+	int	i;
+	char	hex[16] = "0123456789abcdef";
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 32 || str[i] > 126)
+		{
+			write(1, "\\", 1);
+			write(1, &hex[(unsigned char)str[i] / 16], 1);
+			write(1, &hex[(unsigned char)str[i] % 16], 1);
+		}
+		else
+		{
+			write(1, &str[i], 1);
+		}
+		i++;
+	}
 }
+/*
+int main(void)
+{
+    char str[] = "Coucou\ntu vas bien ?";
+    ft_putstr_non_printable(str);
+    return 0;
+}*/
