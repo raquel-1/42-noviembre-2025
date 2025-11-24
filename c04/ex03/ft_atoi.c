067512890691ddaf49728c164ff6b20d68024dcf
raqroca- <raqroca-@student.42madrid.com>
2025-11-24 12:48:21 +0100

    c04 corregido

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,8 +6,40 @@
 /*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/20 16:39:50 by raqroca-          #+#    #+#             */
-/*   Updated: 2025/11/20 16:41:15 by raqroca-         ###   ########.fr       */
+/*   Updated: 2025/11/21 13:03:07 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 
+int	ft_atoi(char *str)
+{
+	long	n;
+	int		sig;
+	int		i;
+
+	n = 0;
+	sig = 1;
+	i = 0;
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sig *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = (n * 10) + (str[i] - '0');
+		i++;
+	}
+	return (n * sig);
+}
+/*
+int   main()
+{
+    char str[50] = "    --+--+1234ab567";
+    int nb = ft_atoi(str);
+    printf("%d\n", nb);
+}*/
