5a2ef780b13401c9cbaf8d64638f6645f62f5516
raqroca- <raqroca-@student.42madrid.com>
2025-11-24 13:33:05 +0100

    c02 todos bien

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -5,25 +5,60 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/17 15:07:42 by raqroca-          #+#    #+#             */
-/*   Updated: 2025/11/17 15:26:27 by raqroca-         ###   ########.fr       */
+/*   Created: 2025/11/24 10:39:45 by raqroca-          #+#    #+#             */
+/*   Updated: 2025/11/24 12:26:05 by raqroca-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 /*
- #include <stdio.h>
-*/
+If you do not meet the condition, I will return the character.*/
+
+char	ft_to_upper(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+char	ft_to_lower(char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
+
+int	ft_is_alphanumeric(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+
+/*
+if (!in_word) -> in_word == 0*/
+
 char	*ft_strcapitalize(char *str)
 {
 	int	i;
+	int	in_word;
 
 	i = 0;
+	in_word = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z' && str[i - 1] < 48)
-			str[i] = str[i] - 32;
-		if (str[i] >= 'A' && str[i] <= 'Z' && str[i - 1] > 48)
-			str[i] = str[i] + 32;
+		if (ft_is_alphanumeric(str[i]))
+		{
+			if (!in_word)
+				str[i] = ft_to_upper(str[i]);
+			else
+				str[i] = ft_to_lower(str[i]);
+			in_word = 1;
+		}
+		else
+			in_word = 0;
 		i++;
 	}
 	return (str);
@@ -31,9 +66,8 @@ char	*ft_strcapitalize(char *str)
 /*
 int	main(void)
 {
-	char src[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
-	printf("before: %s\n", src);
-	ft_strcapitalize(src);
-	printf("after: %s\n", src);
-	return (0);
+    char str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+    printf("Original: %s\n", str);
+    printf("Capitalizado: %s\n", ft_strcapitalize(str));
+    return (0);
 }*/
